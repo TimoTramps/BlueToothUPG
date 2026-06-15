d215262eddf3888ee76c923dbb3db0d1ac9d75d5
Timotej Trampuz <timotej.trampuz@tscng.org>
2026-06-15 10:35:41 +0200

    Updated stuff

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 #include "BluetoothSerial.h"
 #include <Arduino.h>
+#include <DallasTemperature.h>
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -13,21 +14,64 @@
 
 BluetoothSerial SerialBT;
 
+// GPIO where LED is connected to
+const int ledPin =  25;
+
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 32;          
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// Handle received and sent messages
+String message = "";
+char incomingChar;
+String temperatureString = "";
+
+// Timer: auxiliar variables
+unsigned long previousMillis = 0;    // Stores last time temperature was published
+const long interval = 10000;         // interval at which to publish sensor readings
+
+
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
+  pinMode(ledPin, OUTPUT);
   Serial.begin(115200);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
+  // Bluetooth device name
+  SerialBT.begin("ESP32");
   Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
 void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
+  unsigned long currentMillis = millis();
+  // Send temperature readings
+  if (currentMillis - previousMillis >= interval){
+    previousMillis = currentMillis;
+    sensors.requestTemperatures(); 
+    temperatureString = String(sensors.getTempCByIndex(0)) + "C  " +  String(sensors.getTempFByIndex(0)) + "F";
+    SerialBT.println(temperatureString); 
   }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());  }
+  // Read received messages (LED control command)
+  if (SerialBT.available()){
+    char incomingChar = SerialBT.read();
+    if (incomingChar != '\n'){
+      message += String(incomingChar);
+    }
+    else{
+      message = "";
+    }
+    Serial.write(incomingChar);  
+  }
+  // Check received message and control output accordingly
+  if (message =="led_on"){
+    digitalWrite(ledPin, HIGH);
+  }
+  else if (message =="led_off"){
+    digitalWrite(ledPin, LOW);
+  }
   delay(20);
 }
 
